d33a875bbefcb40a2f1c26fbbd9e05455f545a97
ak-git-2020 <ka4kumaramit@gmail.com>
2026-01-09 11:13:22 +0530

    first commit

---
+++ ConfigServer/src/test/java/com/ak/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ak.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
